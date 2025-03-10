7c3580b6826dc49b037811ac272b4020dfeca98d
Ali-william <129856704+Ali-william@users.noreply.github.com>
2025-03-10 23:13:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,66 +14,117 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-       {
-        "identifier": "Muzykant v4 SSL",
-        "host": "lavalink_v4.muzykant.xyz",
-        "port": 443,
-        "password": "https://discord.gg/v6sdrD9kPh",
-        "secure": true,
-        "restVersion": "v4",
-        "authorId": "719878365640916993"
-    },
-    {
-        "identifier": "Muzykant v4",
-        "host": "lavalink_v4.muzykant.xyz",
-        "port": 80,
-        "password": "https://discord.gg/v6sdrD9kPh",
-        "secure": false,
-        "restVersion": "v4",
-        "authorId": "719878365640916993"
-    },
-    {
-        "identifier": "INZEWORLD.COM (DE)",
-        "host": "lava.inzeworld.com",
-        "port": 3128,
-        "password": "saher.inzeworld.com",
-        "secure": false,
-        "restVersion": "v4",
-        "authorId": "454863447344021515"
-    },
-    {
-        "identifier": "ChalresNaig Node",
-        "host": "lavahatry4.techbyte.host",
-        "port": 3000,
-        "password": "NAIGLAVA-dash.techbyte.host",
-        "secure": false,
-        "restVersion": "v4",
-        "authorId": "573709909594734603"
-    },
-    {
-        "identifier": "Public Lavalink v4",
-        "host": "lava-v4.ajieblogs.eu.org",
-        "port": 443,
-        "password": "https://dsc.gg/ajidevserver",
-        "secure": true,
-        "restVersion": "v4",
-        "authorId": "867303900884893727"
-    },
-    {
-        port: 3000,
-        password: "saher.inzeworld.com",
-        host: "lava.inzeworld.com",
-        port: 3128,
-        secure: false
-    },
-    {
-        "identifier": "Public Lavalink v4 NonSSL",
-        "host": "lava-v4.ajieblogs.eu.org",
-        "port": 80,
-        "password": "https://dsc.gg/ajidevserver",
-        "secure": false,
-        "restVersion": "v4",
-        "authorId": "867303900884893727"
-    } 
+      {
+            "identifier": "LavalinkHub",
+            "host": "69.30.219.178",
+            "port": 9781,
+            "password": "https://discord.gg/VGMhhY7AAD",
+            "secure": false,
+            "restVersion": "v4",
+            "authorId": "548192290003353631"
+        } ,
+        {
+            "identifier": "Public Lavalink v4",
+            "host": "lava-v4.ajieblogs.eu.org",
+            "port": 443,
+            "password": "https://dsc.gg/ajidevserver",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "867303900884893727"
+        },
+        {
+            "identifier": "Public Lavalink v4 NonSSL",
+            "host": "lava-v4.ajieblogs.eu.org",
+            "port": 80,
+            "password": "https://dsc.gg/ajidevserver",
+            "secure": false,
+            "restVersion": "v4",
+            "authorId": "867303900884893727"
+        },
+
+ {
+            "identifier": "Muzykant v4 SSL",
+            "host": "lavalink_v4.muzykant.xyz",
+            "port": 443,
+            "password": "https://discord.gg/v6sdrD9kPh",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "719878365640916993"
+        },
+        {
+            "identifier": "Muzykant v4",
+            "host": "lavalink_v4.muzykant.xyz",
+            "port": 80,
+            "password": "https://discord.gg/v6sdrD9kPh",
+            "secure": false,
+            "restVersion": "v4",
+            "authorId": "719878365640916993"
+        },
+
+ {
+            "identifier": "INZEWORLD.COM (DE)",
+            "host": "lava.inzeworld.com",
+            "port": 3128,
+            "password": "saher.inzeworld.com",
+            "secure": false,
+            "restVersion": "v4",
+            "authorId": "454863447344021515"
+        },
+
+ {
+            "identifier": "ChalresNaig Node",
+            "host": "lavahatry4.techbyte.host",
+            "port": 3000,
+            "password": "NAIGLAVA-dash.techbyte.host",
+            "secure": false,
+            "restVersion": "v4",
+            "authorId": "573709909594734603"
+        },
+
+{
+            "identifier": "Disutils Team Lavalink 1",
+            "host": "lavalink-1.is-it.pink",
+            "port": 443,
+            "password": "https://disutils.com",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "418941954252996609"
+        },
+        {
+            "identifier": "Disutils Team Lavalink 2",
+            "host": "lavalink-2.is-it.pink",
+            "port": 443,
+            "password": "https://disutils.com",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "418941954252996609"
+        },
+        {
+            "identifier": "Disutils Team Lavalink 3",
+            "host": "lavalink-3.is-it.pink",
+            "port": 443,
+            "password": "https://disutils.com",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "418941954252996609"
+        },
+        {
+            "identifier": "Disutils Team Lavalink 4",
+            "host": "lavalink-4.is-it.pink",
+            "port": 443,
+            "password": "https://disutils.com",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "418941954252996609"
+        },
+        {
+            "identifier": "Disutils Team Lavalink 5",
+            "host": "lavalink-5.is-it.pink",
+            "port": 443,
+            "password": "https://disutils.com",
+            "secure": true,
+            "restVersion": "v4",
+            "authorId": "418941954252996609"
+        }
   ]
 }
